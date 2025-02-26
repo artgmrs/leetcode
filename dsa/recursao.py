a7d6aa735e89ad5d23d89ee33e2fb2292edc4ca5
Arthur Miranda <arthurdonds@gmail.com>
2025-02-26 11:19:20 -0300

    feat: recursao

---
+++ dsa/recursao.py
@@ -0,0 +1,11 @@
+# Uma função recurssiva deve possuir um caso base e um caso recursivo
+# A falta do caso base faz a função ser executada em loop infinito 
+
+def fat(x):
+  if x == 1:
+    return 1
+  
+  return x * fat(x-1)
+
+
+print(fat(5))

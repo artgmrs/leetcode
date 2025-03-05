0de1f03ad4009b93d54e92e40f2c2ecc279eef9d
Arthur Miranda <arthurdonds@gmail.com>
2025-03-05 10:32:04 -0300

    feat: more quicksort

--- dsa/entendendo_algoritmos_aditya/ordenacao/quick_sort.py
+++ dsa/entendendo_algoritmos_aditya/ordenacao/quick_sort.py
@@ -17,7 +17,7 @@ print("By The Book", quicksort(itens)) # Saída esperada: By The Book [1, 2, 3, 
 # O(n log n)
 # Pegando pivo do meio para evitar pior cenario
 # Retirando iguais ao pivo da recursividade para evitar loop infinito
-def quicksort(arr):
+def quicksort2(arr):
   if len(arr) < 2:
     return arr
   else:
@@ -25,7 +25,35 @@ def quicksort(arr):
     menores = [i for i in arr if i < pivo]
     iguais = [i for i in arr if i == pivo]
     maiores = [i for i in arr if i > pivo]
-    return quicksort(menores) + iguais + quicksort(maiores)
+    return quicksort2(menores) + iguais + quicksort2(maiores)
 
 itens = [3,3,5,2,1,4]
-print("Mais otimizado", quicksort(itens)) # Saída esperada: Mais otimizado [1, 2, 3, 3, 4, 5]
+print("Mais otimizado", quicksort2(itens)) # Saída esperada: Mais otimizado [1, 2, 3, 3, 4, 5]
+
+# O ?
+# Sem usar list comprehension e alterando a lista em si
+
+def start_quicksort3(arr):
+  quicksort3(arr, 0, len(arr) - 1);
+  return arr
+
+def quicksort3(arr, left, right):
+  if left < right:
+    pivo = partition(arr, left, right)
+    quicksort3(arr, left, pivo - 1) # esquerda do pivot
+    quicksort3(arr, pivo + 1 , right) # direita do pivot
+
+def partition(arr, left, right):
+  pivo = arr[right]
+  i = left - 1
+
+  for j in range(left, right):
+    if arr[j] <= pivo:
+      i = i + 1
+      arr[i], arr[j] = arr[j], arr[i]
+
+  arr[i+1], arr[right] = arr[right], arr[i+1] # Achou o lugar do pivo
+  return i+1
+
+itens = [3,3,5,2,1,4]
+print("Usando partition", start_quicksort3(itens))
